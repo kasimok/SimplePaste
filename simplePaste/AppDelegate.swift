a2a1a76f32d94bf2e913c144f8d811b97fb8a938
kasimok <kasimok@163.com>
2022-04-01 08:23:26 +0800

    Initial Commit

---
+++ simplePaste/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  simplePaste
+//
+//  Created by Evilisn on 1/Apr/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ simplePaste/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  simplePaste
+//
+//  Created by Evilisn on 1/Apr/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
